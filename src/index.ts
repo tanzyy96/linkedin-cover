005b5955ff01430af4a9e3dd9ffe6f5af39b9edc
Zhi Yang <zhiyang@kitesense.sg>
2023-04-30 00:34:32 +0800

    init model with decent results

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,34 @@
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
+import { loadQAStuffChain } from "langchain/chains";
+import { Document } from "langchain/document";
+import { TextLoader } from "langchain/document_loaders";
 import { LinkedinLoader } from "linkedinLoader.js";
 
 dotenv.config();
 
-// const model = new OpenAI({
-// 	modelName: "gpt-3.5-turbo",
-// 	openAIApiKey: process.env.OPENAI_API_KEY,
-// });
+const model = new OpenAI({
+	modelName: "gpt-3.5-turbo",
+	openAIApiKey: process.env.OPENAI_API_KEY,
+});
 
 const loader = new LinkedinLoader(
 	"https://sg.linkedin.com/in/zhi-yang-tan-265815166"
 );
 const docs = await loader.load();
 
-console.log(docs);
-// const res = await model.call(
-// 	"What's a good idea for an application to build with GPT-3?"
-// );
+const txtLoader = new TextLoader("jd.txt");
+const jd = await txtLoader.load();
 
-// console.log(res);
+const chain = loadQAStuffChain(model);
+const res = await chain.call({
+	input_documents: [...docs, ...jd],
+	question: `Write a cover letter for this job. 
+		Find relevant experiences in the applicant's resume and talk about how they match the job requirements.
+		Paraphrase what the applicant has written in their resume.
+		Ignore experiences that are not relevant to the job or are not recent.
+		Talk about how relevant experiences have equipped the applicant with the appropriate soft skills.
+		`,
+});
+
+console.log(res);
